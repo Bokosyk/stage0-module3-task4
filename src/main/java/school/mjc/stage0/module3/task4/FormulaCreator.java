38cde75a9797608db0beb3aa7001e5052b0cc831
Leandro Parrado <lparrdev@gmail.com>
2024-07-10 13:32:14 -0400

    TS1

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 97;
+        int b = 98;
+        int nine = 9;
+        int five = 5;
+        int two = 2;
+        int seven = 7;
+        int four = 4;
+
+        System.out.println("(" + nine + (char)a + "^2-" + five + (char)b + "+" + two + "+" + (char)a + "-" + seven + ") * ");
+
     }
 }
